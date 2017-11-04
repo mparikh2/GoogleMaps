6d0d4f45f29490d038e81391b258eba7109dcd22
mparikh2 <milu2409@yahoo.com>
2017-11-03 17:28:51 -0700

    Initial commit

---
+++ app/src/main/java/com/example/milind/mapsdemo/GetDirectionsData.java
@@ -0,0 +1,62 @@
+package com.example.milind.mapsdemo;
+
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by Milind on 11/2/2017.
+ */
+
+public class GetDirectionsData extends AsyncTask<Object, String, String> {
+
+    private String googleDirectionsData;
+    private GoogleMap mMap;
+    String url;
+    String duration, distance;
+    LatLng latLng;
+
+    @Override
+    protected String doInBackground(Object... objects) {
+        mMap = (GoogleMap)objects[0];
+        url = (String)objects[1];
+        latLng = (LatLng) objects[2];
+
+        DownloadURL downloadURL = new DownloadURL();
+        try {
+            googleDirectionsData = downloadURL.readUrl(url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return googleDirectionsData;
+    }
+
+    @Override
+    protected void onPostExecute(String s){
+        HashMap<String,String> directionsList = null;
+        DataParser parser = new DataParser();
+        directionsList = parser.parseDirections(s);
+        duration = directionsList.get("duration");
+        distance = directionsList.get("distance");
+
+        mMap.clear();
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(latLng);
+        markerOptions.draggable(true);
+        markerOptions.title("Duration ="+duration);
+        markerOptions.snippet("Distance ="+distance);
+
+        mMap.addMarker(markerOptions);
+    }
+
+}
